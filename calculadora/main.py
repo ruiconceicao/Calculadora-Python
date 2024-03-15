c9fcf4425c27ac5d1064f41fcdb45c3a22c9e6a5
Rui Conceição <ruifilipeconceicao12@gmail.com>
2024-03-15 13:16:06 +0000

    Update calculadora alpha v0.0.1

--- calculadora/main.py
+++ calculadora/main.py
@@ -12,35 +12,35 @@ class App(ctk.CTk):
         super().__init__()
 
         self.title("Calculadora")
-        self.geometry("400x180")
-        
+
+
         self.resFrame = MyResFrame(self)
-        self.resFrame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="new")
+        self.resFrame.grid(row=0, column=0, sticky="nwe")
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+
 
         self.buttonFrame = MyButtonFrame(self)
-        self.buttonFrame.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="new")
+        self.buttonFrame.grid(row=1, column=0, sticky="nw")
         self.grid_columnconfigure(1, weight=1)
-        self.grid_rowconfigure(1, weight=1)
+
 
 class MyResFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.resLabel = ctk.CTkLabel(self, text="0")
-        self.resLabel.pack(expand=True, pady=20, padx=20)
+        self.resLabel = ctk.CTkLabel(self, text="0", font=("Console", 50))
+        self.resLabel.pack(expand=True, anchor="se")
 
 class MyButtonFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.btn0 = ctk.CTkButton(self, text="0", command=lambda: self.button_callback(0))
-        self.btn0.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
-        self.btn1 = ctk.CTkButton(self, text="1", command=lambda: self.button_callback(1))
-        self.btn1.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
-        self.btn2 = ctk.CTkButton(self, text="2", command=lambda: self.button_callback(2))
-        self.btn2.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+        self.btn0 = ctk.CTkButton(self, text="0", corner_radius=100, width=50, height=35, command=lambda: self.button_callback(0))
+        self.btn0.grid(row=3, column=0, sticky="new")
+        self.btn1 = ctk.CTkButton(self, text="1", corner_radius=100, width=50, height=35, command=lambda: self.button_callback(1))
+        self.btn1.grid(row=3, column=1, sticky="new")
+        self.btn2 = ctk.CTkButton(self, text="2", corner_radius=100, width=50, height=35, command=lambda: self.button_callback(2))
+        self.btn2.grid(row=3, column=2, sticky="new")
             
     def button_callback(self, btn_number):
             print("{}".format(btn_number))
