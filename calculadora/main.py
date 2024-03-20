00d75d6b2b7e4da1d5471250a721a872f15dbf0a
Rui Conceição <ruifilipeconceicao12@gmail.com>
2024-03-20 13:49:26 +0000

    Update main.py alpha v0.0.6 Updated button defs Implemented math operations

--- calculadora/main.py
+++ calculadora/main.py
@@ -5,9 +5,8 @@ operações matemáticas servindo então como calculadora.
 Programa criado a 13/03/24 às 13:00
 Rui Conceição
 '''
+from cgitb import text
 import customtkinter as ctk
-
-
 
 class App(ctk.CTk):
     def __init__(self):
@@ -15,54 +14,62 @@ class App(ctk.CTk):
 
         self.title("Calculadora")
         self._set_appearance_mode("dark")
+        self.minsize
+        self.maxsize
 
-        self.buttonFrame = MyButtonFrame(self)
+        self.buttonFrame = MyFrame(self)
         self.buttonFrame.pack(pady=10, padx=10, anchor="nw")
         
-class MyButtonFrame(ctk.CTkFrame):
+class MyFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-
         # Label for result and for last math operation in rows 0 and 1
         self.contaLabel = ctk.CTkLabel(self, text="", font=("Roboto", 30), anchor="se")
-        self.resLabel = ctk.CTkLabel(self, text="0", font=("Roboto", 30), anchor="se")
+        self.resLabel = ctk.CTkLabel(self, text="", font=("Roboto", 30), anchor="se")
         
         # Buttons in column 0
-        self.btnAC = ctk.CTkButton(self, text="AC", font=("Roboto", 30), corner_radius=100, width=0, height=40, fg_color="#808080", text_color="black", border_color="black", border_width=1, hover_color="#a6a6a6")
+        self.btnAC = ctk.CTkButton(self, text="AC", font=("Roboto", 30), corner_radius=100, width=0, height=40, fg_color="#808080", text_color="black", border_color="black", border_width=1, hover_color="#a6a6a6", 
+            command=lambda: self.btn_AC())
         self.btn7 = ctk.CTkButton(self, text="7", font=("Roboto", 30), corner_radius=100, width=0, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn7.cget("text")))
+            command=lambda: self.btn_click(self.btn7.cget("text")))
         self.btn4 = ctk.CTkButton(self, text="4", font=("Roboto", 30), corner_radius=100, width=0, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn4.cget("text")))
+            command=lambda: self.btn_click(self.btn4.cget("text")))
         self.btn1 = ctk.CTkButton(self, text="1", font=("Roboto", 30), corner_radius=100, width=0, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn1.cget("text")))
+            command=lambda: self.btn_click(self.btn1.cget("text")))
         self.btn0 = ctk.CTkButton(self, text="0", font=("Roboto", 30), corner_radius=100, width=0, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn0.cget("text")))
+            command=lambda: self.btn_click(self.btn0.cget("text")))
         
         # Buttons in column 1
         self.btnNeg = ctk.CTkButton(self, text="±", font=("Roboto", 30), corner_radius=100, width=80, height=40, fg_color="#808080", text_color="black", border_color="black", border_width=1, hover_color="#a6a6a6")
         self.btn8 = ctk.CTkButton(self, text="8", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn8.cget("text")))
+            command=lambda: self.btn_click(self.btn8.cget("text")))
         self.btn5 = ctk.CTkButton(self, text="5", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn5.cget("text")))
+            command=lambda: self.btn_click(self.btn5.cget("text")))
         self.btn2 = ctk.CTkButton(self, text="2", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn2.cget("text"))) 
+            command=lambda: self.btn_click(self.btn2.cget("text"))) 
+        
         # Buttons in column 2
         self.btnPerc = ctk.CTkButton(self, text="%", font=("Roboto", 30), corner_radius=100, width=80, height=40, fg_color="#808080", text_color="black", border_color="black", border_width=1, hover_color="#a6a6a6")
         self.btn9 = ctk.CTkButton(self, text="9", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn9.cget("text")))
+            command=lambda: self.btn_click(self.btn9.cget("text")))
         self.btn6 = ctk.CTkButton(self, text="6", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn6.cget("text")))
+            command=lambda: self.btn_click(self.btn6.cget("text")))
         self.btn3 = ctk.CTkButton(self, text="3", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btn3.cget("text")))
+            command=lambda: self.btn_click(self.btn3.cget("text")))
         self.btndot = ctk.CTkButton(self, text=".", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#424242", text_color="white", border_color="black", border_width=1, hover_color="#666666",
-            command=lambda: self.button_callback(self.btndot.cget("text")))
+            command=lambda: self.btn_click(self.btndot.cget("text")))
         
         # Buttons in column 3
-        self.btndivide = ctk.CTkButton(self, text="÷", font=("Roboto", 30), corner_radius=100, width=80, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e")
-        self.btntimes = ctk.CTkButton(self, text="x", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e")
-        self.btnminus = ctk.CTkButton(self, text="-", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e")
-        self.btnplus = ctk.CTkButton(self, text="+", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e")
-        self.btnequal = ctk.CTkButton(self, text="=", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e")
+        self.btndivide = ctk.CTkButton(self, text="÷", font=("Roboto", 30), corner_radius=100, width=80, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e",
+            command=lambda: self.btn_click("/"))
+        self.btntimes = ctk.CTkButton(self, text="x", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e",
+            command=lambda: self.btn_click("*"))
+        self.btnminus = ctk.CTkButton(self, text="-", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e",
+            command=lambda: self.btn_click(self.btnminus.cget("text")))
+        self.btnplus = ctk.CTkButton(self, text="+", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e",
+            command=lambda: self.btn_click(self.btnplus.cget("text")))
+        self.btnequal = ctk.CTkButton(self, text="=", font=("Roboto", 30), corner_radius=100, width=60, height=40, fg_color="#e0810d", text_color="white", border_color="black", border_width=1, hover_color="#f4a23e",
+            command=lambda: self.btn_equal())
 
         # Label configs and positions in rows 0 and 1
         self.contaLabel.grid(row=0, column=0, columnspan=4, sticky="new", pady=1)
@@ -92,8 +99,17 @@ class MyButtonFrame(ctk.CTkFrame):
         self.btndot.grid(row=6, column=2, sticky="new", padx=1, pady=5)
         self.btnequal.grid(row=6, column=3, sticky="new", padx=1, pady=5)
 
-    def button_callback(self, btn_number):
-            self.resLabel.configure(text=("{}{}".format(self.resLabel.cget("text"), btn_number)))
+    def btn_click(self, btn_number):
+        self.resLabel.configure(text=("{}{}".format(self.resLabel.cget("text"), btn_number)))
+
+    def btn_equal(self):
+        self.contaLabel.configure(text=("{}".format(self.resLabel.cget("text"))))
+        self.resLabel.configure(text=("{}".format(eval(self.resLabel.cget("text")))))
+
+    def btn_AC(self):
+        self.contaLabel.configure(text="")
+        self.resLabel.configure(text="")
+        
 
 app = App()
 app.mainloop()
